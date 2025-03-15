94245e54e1b2113baa6ffea8f45c038f2e3fa5ee
Nilver Pulce <pt47266719@idat.edu.pe>
2025-03-15 02:40:39 -0500

    Inicio de proyecto

---
+++ src/main/java/com/ec3/pulcerojas/PulceRojasApplication.java
@@ -0,0 +1,13 @@
+package com.ec3.pulcerojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulceRojasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulceRojasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ec3/pulcerojas/PulceRojasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec3.pulcerojas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulceRojasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
